a4a8d8e293e5cf873694171b693184da57319e4c
Umair Tauni <engrumayr@gmail.com>
2024-04-14 16:23:50 +0500

    Conditional Rendering with &&

--- src/index.js
+++ src/index.js
@@ -70,15 +70,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <div>
         {pizzaData.map((pizza) => (
